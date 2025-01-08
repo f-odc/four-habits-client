f6d6aa8fa96eada94730a74252503737e649ed86
f-odc <f.o.d.c@web.de>
2025-01-08 17:12:49 +0100

    #4 Update habit screen

--- lib/pages/habits/create_habit_screen.dart
+++ lib/pages/habits/create_habit_screen.dart
@@ -18,65 +18,190 @@ class _CreateHabitScreenState extends State<CreateHabitScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Create Habit'),
-      ),
-      body: Form(
-        key: _formKey,
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(60.0),
         child: Column(
-          children: <Widget>[
-            TextFormField(
-              decoration: const InputDecoration(labelText: 'Habit Name'),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter a habit name';
-                }
-                return null;
-              },
-              onSaved: (value) {
-                _habitName = value!;
-              },
+          children: [
+            AppBar(
+              iconTheme: const IconThemeData(color: Colors.orange), // Set back button color to orange
+              title: const Text(
+                'Create Your Habit!',
+                style: TextStyle(
+                  color: Colors.orange,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25,
+                ),
+              ),
+              centerTitle: true, // Center the title
+              elevation: 0, // Add a small shadow
             ),
-            DropdownButtonFormField<String>(
-              value: _occurrenceType,
-              decoration: const InputDecoration(labelText: 'Occurrence'),
-              items: <String>['Daily', 'Weekly', 'Monthly']
-                  .map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-              onChanged: (String? newValue) {
-                setState(() {
-                  _occurrenceType = newValue;
-                });
-              },
-            ),
-            if (_occurrenceType != 'Daily' && _occurrenceType != null)
+          ],
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              // HABIT CARD PREVIEW
+              Card(
+                child: ListTile(
+                  leading: const Icon(Icons.check_circle_outline),
+                  title: Text(
+                    'Habit: $_habitName',
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                  subtitle: Text(
+                    _occurrenceType == 'Daily'
+                        ? 'Occurrence: $_occurrenceType'
+                        : 'Occurrence: $_occurrenceType - ${_occurrenceController.text}',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.grey[600],
+                    ),
+                  ),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      const Text(
+                        '0',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0,
+                        ),
+                      ),
+                      const Icon(Icons.local_fire_department, color: Colors.orange),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+              const Divider(
+                thickness: 2.0,
+                indent: 16.0,
+                endIndent: 16.0,
+                height: 1,
+              ),
+              const SizedBox(height: 16),
+              const Text(
+                'Habit Name',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 2),
               TextFormField(
-                controller: _occurrenceController,
-                decoration: const InputDecoration(labelText: 'Enter number of times'),
-                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Colors.orange),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
                 validator: (value) {
                   if (value == null || value.isEmpty) {
-                    return 'Please enter a number';
+                    return 'Please Enter a Habit Name';
                   }
                   return null;
                 },
+                onSaved: (value) {
+                  _habitName = value!;
+                },
+                onChanged: (value) {
+                  setState(() {
+                    _habitName = value;
+                  });
+                },
               ),
-            ElevatedButton(
-              onPressed: () async {
-                if (_formKey.currentState!.validate()) {
-                  _formKey.currentState!.save();
-                  // save the habit
-                  await _logic.saveHabit(_habitName, _occurrenceType!, _occurrenceController.text);
-                  Navigator.pop(context, 'saved');
-                }
-              },
-              child: const Text('Save Habit'),
-            ),
-          ],
+              const SizedBox(height: 8),
+              const Text(
+                'Occurrence',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 2),
+              DropdownButtonFormField<String>(
+                value: _occurrenceType,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Colors.orange),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+                items: <String>['Daily', 'Weekly', 'Monthly']
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    _occurrenceType = newValue;
+                  });
+                },
+              ),
+              if (_occurrenceType != 'Daily' && _occurrenceType != null)
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: TextFormField(
+                    controller: _occurrenceController,
+                    decoration: InputDecoration(
+                      labelText: 'Please Enter a Number',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Colors.orange),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                    keyboardType: TextInputType.number,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please Enter a Number';
+                      }
+                      return null;
+                    },
+                    onChanged: (value) {
+                      setState(() {});
+                    },
+                  ),
+                ),
+              const SizedBox(height: 16),
+              GestureDetector(
+                onTap: () async {
+                  if (_formKey.currentState!.validate()) {
+                    _formKey.currentState!.save();
+                    // save the habit
+                    await _logic.saveHabit(_habitName, _occurrenceType!, _occurrenceController.text);
+                    Navigator.pop(context, 'saved');
+                  }
+                },
+                child: Card(
+                  color: Colors.orange[100],
+                  child: ListTile(
+                    leading: const Icon(Icons.save, color: Colors.orange),
+                    title: const Text(
+                      'Save Habit',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0,
+                        color: Colors.orange,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
