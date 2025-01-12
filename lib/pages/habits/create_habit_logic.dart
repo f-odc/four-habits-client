8461ecd4bc70d1b7b1859ab0bb07083ef3a8af82
f-odc <f.o.d.c@web.de>
2025-01-12 17:12:23 +0100

    Fix missing value error by adding default

--- lib/pages/habits/create_habit_logic.dart
+++ lib/pages/habits/create_habit_logic.dart
@@ -5,8 +5,14 @@ import '../../services/shared_preferences_service.dart';
 class CreateHabitLogic {
   final SharedPreferencesService _prefsService = SharedPreferencesService();
 
-  Future<void> saveHabit(String habitName, String occurrenceType, String occurrenceNum) async {
+  Future<void> saveHabit(
+      String habitName, String occurrenceType, String occurrenceNum) async {
     await _prefsService.init();
+
+    // Default occurrenceNum to '1' if it is empty
+    if (occurrenceNum.isEmpty) {
+      occurrenceNum = '1';
+    }
 
     // Create a new Habit object
     Habit habit = Habit(
